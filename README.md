5272d917429c47add354e08c7b8224813dc4b0fe
Carlos-Fer-MP <crojasfer31@gmail.com>
2022-02-21 18:35:13 +0100

    Fix: Cambio de dependencias

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Clorophyll
 
-Este proyecto implementa una api contruida sobre Express,Mongodb y Node.
+Este proyecto implementa una api contruida sobre Express, Mongodb y Node.
 
 Aparte de un apartado Front-End construido con Vue.js.
 
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,21 +1,23 @@
 
 module.exports = app => {
   const productos = require('../controllers/controller.js')
-
+  //
   const router = require('express').Router()
-
+  //
   router.post('/', productos.create)
-
+  //
   router.get('/', productos.findAll)
 
   // Encontrar uno especifico.
-
   router.get('/', productos.findOne)
 
+  //
   router.put('/', productos.update)
 
+  //
   router.delete('/', productos.delete)
 
+  //
   router.delete('/', productos.deleteAll)
 
   app.use('/api/productos', router)
